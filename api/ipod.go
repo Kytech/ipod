0ef036e50c90f9a09cf3096db254623ad01cb71b
Kytech <Kytech@users.noreply.github.com>
2021-07-15 21:49:36 -0600

    Add currently implemented methods to ipod API

--- api/ipod.go
+++ api/ipod.go
@@ -1,8 +1,19 @@
 package api
 
 type Ipod interface {
+	Name() string
+
 	SetPlayerStatePlaying()
+	SetPlayerStatePaused()
+	SetPlayerStateStopped()
+	SetPlayerStateError()
+
+	ChapterName() string
+	SetChapterName(string)
+	TrackTitle() string
 	SetTrackTitle(string)
+	TrackArtist() string
 	SetTrackArtist(string)
+	TrackAlbum() string
 	SetTrackAlbum(string)
 }
